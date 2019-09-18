4a8d24562334fb2a2b8234fda1a68c1f74072f1a
Leomar da Silva Barbosa <leomar.barbosa@kaiostech.com>
2019-09-18 17:53:27 -0400

    update

--- build/alpine.sh
+++ build/alpine.sh
@@ -1,9 +1,8 @@
-echo "http://dl-4.alpinelinux.org/alpine/v3.10/main" >> /etc/apk/repositories && \
-echo "http://dl-4.alpinelinux.org/alpine/v3.10/community" >> /etc/apk/repositories
+echo "http://dl-4.alpinelinux.org/alpine/v3.9.4/main" >> /etc/apk/repositories && \
+echo "http://dl-4.alpinelinux.org/alpine/v3.9.4/community" >> /etc/apk/repositories
 
 apk update && \
     apk add build-base \
-    apk --update add --no-cache --virtual run-dependencies \
     libxml2-dev \
     libxslt-dev \
     postgresql-dev \
